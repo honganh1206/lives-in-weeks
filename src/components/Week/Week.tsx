804451f05e342ac94aefcb4f05a99c3e1a2cf010
Hong Anh Pham <phamhonganh12062000@gmail.com>
2025-05-23 17:27:31 +0700

    WIP Weeks

--- src/components/Week/Week.tsx
+++ src/components/Week/Week.tsx
@@ -3,6 +3,7 @@ import { useSearchParams } from "next/navigation";
 import { useRouter } from "next/router";
 import React from "react";
 import Tooltip from "../Tooltip";
+import styles from "./week.module.css";
 
 function formatDate(date: string, showYear?: boolean) {
   return new Date(date).toLocaleDateString("en-US", {
@@ -21,7 +22,7 @@ export interface IWeek {
 
 // Skip re-rendering when props are unchanged with memo
 const MemoizedWeek = React.memo(function Week({ week }: { week: IWeek }) {
-  // Object destructuring with aliasing, we bind the lifeEvent with a local var
+  // Object de-structuring with aliasing, we bind the lifeEvent with a local var
   const { sunday, saturday, lifeEvent: existingLifeEvent } = week;
 
   const router = useRouter();
@@ -29,12 +30,52 @@ const MemoizedWeek = React.memo(function Week({ week }: { week: IWeek }) {
 
   const [lifeEvent, setLifeEvent] = React.useState(existingLifeEvent);
 
+  // Update the URL based on a newEvent
+  // 100ms to prevent excessive calls to router.push e.g., during rapid typing
   const handleUrlUpdate = debounce((newEvent: string) => {
     const newParams = new URLSearchParams(Array.from(searchParams.entries()));
-  });
+    const sundayKey = new Date(sunday).toISOString().split("T")[0];
+
+    newEvent
+      ? newParams.set(sundayKey, encodeURIComponent(newEvent))
+      : newParams.delete(sundayKey);
+
+    router.push(`?${newParams.toString()}`);
+  }, 100);
+
+  function handleLifeEventChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const newEvent = e.target.value;
+    setLifeEvent(newEvent);
+    handleUrlUpdate(newEvent);
+  }
+
+  // Reference a value that is not needed for rendering
+  // Changing it does not trigger a re-render
+  // Store information between re-renders
+  const inputRef = React.useRef<HTMLInputElement>(null);
+
+  // No way to set dynamic width for input
+  // One ch unit is the size of a 0 char
+  React.useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.style.width = `${Math.max(1, lifeEvent.length + 2)}ch`;
+    }
+  }, [lifeEvent]);
 
   return (
-    <Tooltip trigger={<input></input>}>
+    <Tooltip
+      trigger={
+        <input
+          ref={inputRef}
+          type="text"
+          className={styles.week}
+          value={lifeEvent}
+          data-empty={!lifeEvent}
+          onChange={handleLifeEventChange}
+          maxLength={100}
+        ></input>
+      }
+    >
       <p>
         {formatDate(sunday)} to {formatDate(saturday, true)}
       </p>
--- src/hooks/useBirthdate.hook.ts
+++ src/hooks/useBirthdate.hook.ts
@@ -1,6 +1,22 @@
 import { useSearchParams } from "next/navigation";
 import { useRouter } from "next/router";
+import React from "react";
+import { IWeek } from "@/components/Week";
 
+export const DECADE_LABELS = [
+  "Childhood",
+  "Teens",
+  "20s",
+  "30s",
+  "40s",
+  "50s",
+  "60s",
+  "70s",
+  "80s",
+  "90s",
+];
+
+// Generate a nested array of weeks grouped by decade
 export function useBirthdate(bday: string | undefined) {
   const router = useRouter();
   const searchParams = useSearchParams();
@@ -10,7 +26,75 @@ export function useBirthdate(bday: string | undefined) {
   function handleBirthdateChange(event: React.ChangeEvent<HTMLInputElement>) {
     const newBday = event.target.value;
     setBirthdate(newBday);
+
+    const newParams = new URLSearchParams(Array.from(searchParams.entries()));
+    newParams.set("birthdate", newBday);
+    router.push(`?${newParams.toString()}`);
   }
 
-  const newParams = new URLSearchParams(Array.from(searchParams.entries()));
+  // Generate a nested array of weeks grouped by decades
+  // Only when we update the birthdate will we trigger a re-render
+  const decades = React.useMemo(() => {
+    // Compute each week in a century, grouped by decade
+    const tenDecades = [];
+    const birthdateUTC = new Date(`${birthdate}T00:00:00Z`);
+
+    for (let decade = 0; decade < 10; decade++) {
+      // Compute decade start date
+      const decadeStart = new Date(birthdateUTC);
+      decadeStart.setUTCFullYear(decadeStart.getUTCFullYear() + decade * 10);
+
+      // Compute decade end date
+      const decadeEnd = new Date(decadeStart);
+      decadeEnd.setUTCFullYear(decadeEnd.getUTCFullYear() + 10);
+
+      const sundayBeforeDecadeStart = new Date(decadeStart);
+      sundayBeforeDecadeStart.setUTCDate(
+        decadeStart.getUTCDate() - decadeStart.getUTCDay(),
+      );
+
+      // Generate weeks ahead
+      const decadeWeeks: IWeek[] = [];
+      const sunday = new Date(sundayBeforeDecadeStart);
+      const saturday = new Date(sunday);
+      const birthday = new Date(birthdateUTC);
+
+      saturday.setUTCDate(saturday.getUTCDate() + 6);
+
+      // Loop through all weeks in the decade, creating week objects
+      // Each iteration creates one week from Sunday to Saturday
+      while (saturday < decadeEnd) {
+        const year = sunday.getUTCFullYear();
+        birthday.setUTCFullYear(year);
+
+        const isBirthdayWeek = birthday >= sunday && birthday <= saturday;
+        // Fill the birthday event with age
+        const birthdayEvent = isBirthdayWeek
+          ? `${year - birthdateUTC.getUTCFullYear()} in ${year}`
+          : "";
+
+        // Fill the week events
+        const weekKey = new Date(sunday).toISOString().split("T")[0];
+        const weekEvent = searchParams.get(weekKey);
+        const lifeEvent = weekEvent
+          ? decodeURIComponent(weekEvent)
+          : birthdayEvent;
+
+        decadeWeeks.push({
+          sunday: sunday.toUTCString(),
+          saturday: saturday.toUTCString(),
+          lifeEvent,
+        });
+
+        sunday.setUTCDate(sunday.getUTCDate() + 7);
+        saturday.setUTCDate(saturday.getUTCDate() + 7);
+      }
+
+      tenDecades.push(decadeWeeks);
+    }
+
+    return tenDecades;
+  }, [searchParams, birthdate]);
+
+  return { birthdate, handleBirthdateChange, decades };
 }
